9a09a2c83d061b0747f88a6dfe81415f89df0ab1
dishanta <dishanta.sarma@trojans.dsu.edu>
2025-04-22 13:53:18 -0500

    goon

--- final.cpp
+++ final.cpp
@@ -41,13 +41,4 @@ void Final::loadWordsIntoHashTable(const vector<string>& filenames, HashTable& t
     }
 }
 
-int main() {
-    vector<string> files = {"BeaverChefs.txt", "DogWizards.txt", "EvilCats.txt", "EvilFox.txt", "ForestOwl.txt", "FutureRabbit.txt", "GnomeNecromancy.txt", "LionRoar.txt", "MarsUprising.txt", "MonkeyBusiness.txt", "StormTurtle.txt", "TortoiseandHare.txt"};
-    HashTable myTable(1000, CHAINING_BST); // or any other variant
 
-    Final a; 
-
-    a.loadWordsIntoHashTable(files, myTable);
-
-    myTable.displayStats(); // Optional
-}
---
+++ mainFinal.cpp
@@ -0,0 +1,12 @@
+#include "final.h"
+
+int main() {
+    vector<string> files = {"BeaverChefs.txt", "DogWizards.txt", "EvilCats.txt", "EvilFox.txt", "ForestOwl.txt", "FutureRabbit.txt", "GnomeNecromancy.txt", "LionRoar.txt", "MarsUprising.txt", "MonkeyBusiness.txt", "StormTurtle.txt", "TortoiseandHare.txt"};
+    HashTable myTable(1000, CHAINING_BST); // or any other variant
+
+    Final a; 
+
+    a.loadWordsIntoHashTable(files, myTable);
+
+    myTable.displayStats(); // Optional
+}
