5a7dad52709373e3e883c16ec7bcb8840139003d
dishanta <dishanta.sarma@trojans.dsu.edu>
2025-04-24 20:23:27 -0500

    dswWorks

--- mainFinal.cpp
+++ mainFinal.cpp
@@ -10,5 +10,14 @@ int main() {
 
     myTable.printTable();
 
-    
+    set<string> inputWords = a.readWordsFromFile("testfile.txt");
+
+    vector<string> existingDocs = files;
+
+    cout << "\nSimilarity Scores (lower = more similar):\n";
+    for (const string& doc : existingDocs) {
+        set<string> docWords = a.readWordsFromFile(doc);
+        int score = a.calculateScore(inputWords, docWords);
+        cout << doc << ": " << score << endl;
+    }
 }
