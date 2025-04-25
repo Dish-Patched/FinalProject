b0ea13a472020b78297fe890b246e1f706c2ed72
Sooup24 <drewrian05@gmail.com>
2025-04-25 18:11:28 -0500

    dswIN

--- Makefile
+++ Makefile
@@ -4,18 +4,18 @@ INCLUDE_DIRS := Includes
 CC 			 := g++
 CFLAGS 		 := -I$(INCLUDE_DIRS)
 
-OBJS 		 := avl.o avlHash.o binomial.o final.o hash.o mainFinal.o
-DEPS 		 := ${INCLUDE_DIRS}/avl.h ${INCLUDE_DIRS}/binomial.h ${INCLUDE_DIRS}/avlHash.h ${INCLUDE_DIRS}/final.h ${INCLUDE_DIRS}/hash.h
+OBJS 		 := dsw.o avlHash.o binomial.o final.o hash.o mainFinal.o
+DEPS 		 := ${INCLUDE_DIRS}/dsw.h ${INCLUDE_DIRS}/binomial.h ${INCLUDE_DIRS}/avlHash.h ${INCLUDE_DIRS}/final.h ${INCLUDE_DIRS}/hash.h
 
 .PHONY: clean all
 
-all: avl.o avlHash.o binomial.o final.o hash.o mainFinal.o finalrun
+all: dsw.o avlHash.o binomial.o final.o hash.o mainFinal.o finalrun
 
 clean:
 	rm $(OBJS) finalrun
 
 
-avl.o: avl.cpp ${DEPS}
+dsw.o: dsw.cpp ${DEPS}
 	$(CC) -c -o $@ $(CFLAGS) $<
 
 avlHash.o: avlHash.cpp ${DEPS}
--- mainFinal.cpp
+++ mainFinal.cpp
@@ -1,23 +1,43 @@
 #include "final.h"
 
 int main() {
-    vector<string> files = {"BeaverChefs.txt", "DogWizards.txt", "EvilCats.txt", "EvilFox.txt", "ForestOwl.txt", "FutureRabbit.txt", "GnomeNecromancy.txt", "LionRoar.txt", "MarsUprising.txt", "MonkeyBusiness.txt", "StormTurtle.txt", "TortoiseandHare.txt"};
-    HashTable myTable(653); 
+    vector<string> files = {
+        "BeaverChefs.txt", "DogWizards.txt", "EvilCats.txt", "EvilFox.txt",
+        "ForestOwl.txt", "FutureRabbit.txt", "GnomeNecromancy.txt", "LionRoar.txt",
+        "MarsUprising.txt", "MonkeyBusiness.txt", "StormTurtle.txt", "TortoiseandHare.txt"
+    };
 
-    Final a; 
+    HashTable myTable(653);
+    Final a;
+    BST plagiarismTree;
 
     a.loadWordsIntoHashTable(files, myTable);
 
-    myTable.printTable();
-
     set<string> inputWords = a.readWordsFromFile("testfile.txt");
 
-    vector<string> existingDocs = files;
 
-    cout << "\nSimilarity Scores (lower = more similar):\n";
-    for (const string& doc : existingDocs) {
+        // //cout << "\nSimilarity Scores (lower = more similar):\n";
+    // for (const string& doc : existingDocs) {
+    //     set<string> docWords = a.readWordsFromFile(doc);
+    //     int score = a.calculateScore(inputWords, docWords);
+    //     //cout << doc << ": " << score << endl;
+    // }
+
+    // Calculate similarity scores and insert into the DSW tree
+    for (const string& doc : files) {
         set<string> docWords = a.readWordsFromFile(doc);
         int score = a.calculateScore(inputWords, docWords);
-        cout << doc << ": " << score << endl;
+
+        // Insert into the tree: (key = score as string, value = filename)
+        plagiarismTree.insert(to_string(score), doc);
     }
+
+    // Balance the tree after all insertions
+    plagiarismTree.dswBalance();
+
+    // Display the balanced tree
+    cout << "\nFinal Balanced Tree (Score : Filename):" << endl;
+    plagiarismTree.display();
+
+    return 0;
 }
