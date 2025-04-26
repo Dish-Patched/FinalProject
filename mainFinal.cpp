f399b5cdf8dc39c2028771aaefbaf3088444f863
Sooup24 <drewrian05@gmail.com>
2025-04-26 16:41:01 -0500

    FolderCleanup

--- mainFinal.cpp
+++ mainFinal.cpp
@@ -2,39 +2,42 @@
 
 int main() {
     vector<string> files = {
-        "BeaverChefs.txt", "DogWizards.txt", "EvilCats.txt", "EvilFox.txt",
-        "ForestOwl.txt", "FutureRabbit.txt", "GnomeNecromancy.txt", "LionRoar.txt",
-        "MarsUprising.txt", "MonkeyBusiness.txt", "StormTurtle.txt", "TortoiseandHare.txt"
+        "BeaverChefs.txt", "DogWizards.txt", "EvilCats.txt",
+        "EvilFox.txt", "ForestOwl.txt", "FutureRabbit.txt",
+        "GnomeNecromancy.txt", "LionRoar.txt", "MarsUprising.txt",
+        "MonkeyBusiness.txt", "StormTurtle.txt", "TortoiseandHare.txt"
     };
+
+    string folder = "Documents/";
 
     HashTable myTable(653);
     Final a;
     BST plagiarismTree;
 
-    a.loadWordsIntoHashTable(files, myTable);
+    vector<string> fullPaths;
+    for (const string& f : files) {
+        fullPaths.push_back(folder + f);
+    }
+    a.loadWordsIntoHashTable(fullPaths, myTable);
 
-    set<string> inputWords = a.readWordsFromFile("testfile.txt");
+    set<string> inputWords = a.readWordsFromFile(folder + "testfile.txt");
 
-    //cout << "\nSimilarity Scores (lower = more similar):\n";
     for (const string& doc : files) {
-        set<string> docWords = a.readWordsFromFile(doc);
+        set<string> docWords = a.readWordsFromFile(folder + doc);
         int score = a.calculateScore(inputWords, docWords);
-        //cout << doc << ": " << score << endl;
         plagiarismTree.insert(score, doc);
     }
-    // Balance the tree after all insertions
+
     plagiarismTree.dswBalance();
 
-
     int user = 1;
-    while(user != 0)
-    {
+    while (user != 0) {
         cout << "Actions menu: " << endl;
         cout << "Lower scores = More similar. Higher scores = Less similar" << endl;
-        cout << "1. View highest and lowest scores." <<endl;
-        cout << "2. See tree of all scores." <<endl;
-        cout << "3. See scores higher than..." <<endl;
-        cout << "4. See scores lower than..." <<endl;
+        cout << "1. View highest and lowest scores." << endl;
+        cout << "2. See tree of all scores." << endl;
+        cout << "3. See scores higher than..." << endl;
+        cout << "4. See scores lower than..." << endl;
         cout << "0. To exit." << endl;
         cout << endl << "What would you like to do? ";
 
@@ -42,29 +45,30 @@ int main() {
 
         int userin;
 
-        if(user == 1){
-        plagiarismTree.printMax();
+        if (user == 1) {
+            plagiarismTree.printMax();
         }
-        else if(user == 2){
+        else if (user == 2) {
             cout << "\nFinal Balanced Tree (Score : Filename):" << endl;
             plagiarismTree.display();
         }
-        else if(user == 3){
+        else if (user == 3) {
             cout << "Please enter a score to search: ";
             cin >> userin;
             plagiarismTree.findHigher(userin);
         }
-        else if(user == 4){
+        else if (user == 4) {
             cout << "Please enter a score to search: ";
             cin >> userin;
             plagiarismTree.findLower(userin);
         }
-        else if(user == 0){
-            cout << "Exitting..." << endl;
+        else if (user == 0) {
+            cout << "Exiting..." << endl;
             break;
         }
-        else
-            cout <<"Invalid input, please try again" << endl << endl;
+        else {
+            cout << "Invalid input, please try again" << endl << endl;
+        }
     }
 
     return 0;
